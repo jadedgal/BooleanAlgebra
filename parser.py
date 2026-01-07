69a93cd840b381f09dbdcdb4a246dcff2a959edb
Jade E <dhengland44@gmail.com>
2026-01-07 23:00:18 +0000

    Initial Commit

---
+++ parser.py
@@ -0,0 +1,10 @@
+class parser:
+    def __init__(self):
+        self.string = ""
+    
+    def parse(self, string):
+        self.string = string
+        return
+    
+    def stringTokens(self):
+        return
